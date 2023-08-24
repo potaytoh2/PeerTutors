1af96876d97588810dc16df68b41471f5501207f
FiddleMe <bryenyeoh1@gmail.com>
2023-08-24 20:54:00 +0800

    add docker compose migrations

---
+++ backend/db/src/default/migrations/1692881180159-addUserTable.ts
@@ -0,0 +1,12 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class addUserTable1692881180159 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
