d781bc90eddae9a9c2830e4b0eb0847184df2bfd
Axel <axelwong153@gmail.com>
2023-08-26 02:47:23 +0800

    Added header and footer component. Created a home page

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,5 @@
+function Footer(){
+    return <div className='layout-header'>Footer</div>
+}
+
+export default Footer;
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import {useState, useEffect} from ''
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Signup() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
