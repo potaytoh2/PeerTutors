72dcb291ea75dba1bc56c8a708ccff7172a14046
FiddleMe <bryenyeoh1@gmail.com>
2023-09-12 22:55:34 +0800

    l

--- backend/services/src/crud/base.crud.service.ts
+++ backend/services/src/crud/base.crud.service.ts
@@ -4,48 +4,58 @@ import { plainToClass } from 'class-transformer';
 import { DeepPartial } from 'typeorm';
 
 export class BaseCrudService<T> extends TypeOrmCrudService<T> {
+  // Create a new entity
   public async createOne(req: CrudRequest, dto): Promise<T> {
     const { returnShallow } = req.options.routes.createOneBase;
-    const entity = this.prepareEntityBeforeSave(dto, req.parsed);
-    if (!entity) {
+    const entityToSave = this.prepareEntityBeforeSave(dto, req.parsed);
+
+    if (!entityToSave) {
       this.throwBadRequestException('Empty data. Nothing to save.');
     }
-    const saved = await this.repo.save<any>(entity);
-    return saved;
+
+    const savedEntity = await this.repo.save<any>(entityToSave);
+    return savedEntity;
   }
 
+  // Update an existing entity
   public async updateOne(req: CrudRequest, dto): Promise<T> {
     const { allowParamsOverride, returnShallow } =
       req.options.routes.updateOneBase;
+
     const paramsFilters = this.getParamFilters(req.parsed);
-    const found = await this.getOneOrFail(req, returnShallow);
-    const toSave = !allowParamsOverride
-      ? { ...found, ...dto, ...paramsFilters, ...req.parsed.authPersist }
-      : { ...found, ...dto, ...req.parsed.authPersist };
-    const updated = await this.repo.save(
-      plainToClass(this.entityType, toSave) as unknown as DeepPartial<T>,
+    const foundEntity = await this.getOneOrFail(req, returnShallow);
+
+    const updatedEntity = !allowParamsOverride
+      ? { ...foundEntity, ...dto, ...paramsFilters, ...req.parsed.authPersist }
+      : { ...foundEntity, ...dto, ...req.parsed.authPersist };
+
+    const savedEntity = await this.repo.save(
+      plainToClass(this.entityType, updatedEntity) as unknown as DeepPartial<T>,
     );
 
-    return updated;
+    return savedEntity;
   }
 
-  // fix bug nestjsx/crud
+  // Fix a bug related to field aliases in nestjsx/crud
   protected getFieldWithAlias(field: string, sort = false) {
-    const cols = field.split('.');
-    switch (cols.length) {
+    const columns = field.split('.');
+
+    switch (columns.length) {
       case 1:
         return this.alias + '.' + field;
       case 2:
         return field;
       default:
-        return cols.slice(cols.length - 2, cols.length).join('.');
+        return columns.slice(columns.length - 2, columns.length).join('.');
     }
   }
 
+  // Save an entity
   async save(data) {
     return this.repo.save(data);
   }
 
+  // Delete an entity by its ID
   async delete(id: string) {
     return this.repo.delete(id);
   }
--- backend/services/src/crud/base.entity.ts
+++ backend/services/src/crud/base.entity.ts
@@ -1,22 +1,21 @@
-import {BeforeInsert, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn} from 'typeorm';
-import {InternalServerErrorException} from '@nestjs/common';
-import {ApiProperty} from '@nestjs/swagger';
+import { BeforeInsert, PrimaryGeneratedColumn } from 'typeorm';
+import { InternalServerErrorException } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class BaseEntity {
-    @PrimaryGeneratedColumn('uuid')
-    @ApiProperty({
-        readOnly: true,
-        example: '17e3e236-aadf-4131-833c-2d9a0031eee2',
-        description: 'Primary Key UUID',
-    })
-    id?: string;
+  @PrimaryGeneratedColumn('uuid')
+  @ApiProperty({
+    readOnly: true,
+    example: '17e3e236-aadf-4131-833c-2d9a0031eee2',
+    description: 'Primary Key UUID',
+  })
+  id?: string;
 
-    @BeforeInsert()
-    checkUUID() {
-        if (this.id) {
-            throw new InternalServerErrorException('Invalid ID');
-        }
+  // Before inserting a new record, check if ID already exists
+  @BeforeInsert()
+  checkUUID() {
+    if (this.id) {
+      throw new InternalServerErrorException('Invalid ID');
     }
-
-
+  }
 }
